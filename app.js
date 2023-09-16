a271dcd24f789e47262c4295661fa6b0755ab804
Redoou8 <redouane3108@hotmail.com>
2023-09-16 23:49:31 +0200

    Alert info added

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 
+document.addEventListener("DOMContentLoaded", function() {
 
-
+const closeBtn = document.querySelector('.closebtn');
+closeBtn.addEventListener("click", function() {
+  var div = this.parentElement;
+  div.style.opacity = "0";
+  setTimeout(function() {
+    div.style.display = "none";
+  }, 600);
+});
+})
 
 function afficher(val){ 
 
@@ -14,20 +23,13 @@ function afficher(val){
 
 function calcul(){ // touche '='
 
-    var a = document.querySelector(".ecran").value; 
-   
-     
-
-    
+    var a = document.querySelector(".ecran").value;  
     document.querySelector(".ecran").value = eval(a);
-    
-
-
-
-
 }
+
 
 function sqr(){
 
     document.querySelector(".ecran").value = Math.sqrt(document.querySelector(".ecran").value)
 }
+
