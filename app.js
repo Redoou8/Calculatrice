3e91115f284bee521c2711c47f2f9aa6ffcc376a
Redoou8 <redouane3108@hotmail.com>
2023-09-17 03:48:21 +0200

    some comms added

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 
+
+function afficher(val){     
+   document.querySelector(".ecran").value += val; }
+
+
+   function removescreen(){
+    document.querySelector(".ecran").value=""
+}
+
+function calcul(){ 
+     var a = document.querySelector(".ecran").value;  
+    document.querySelector(".ecran").value = eval(a);
+}
+
+
+function sqr(){
+    document.querySelector(".ecran").value = Math.sqrt(document.querySelector(".ecran").value)
+}
+
+
+
+
+
+//____________________________________________________not in "less js possinle challenge"____________________________________________
+
 document.addEventListener("DOMContentLoaded", function() { //utilisation async meilleure ? 
 
 const closeBtn = document.querySelector('.closebtn');
@@ -10,26 +35,3 @@ closeBtn.addEventListener("click", function() {
   }, 600);
 });
 })
-
-function afficher(val){ 
-
-        
-   document.querySelector(".ecran").value += val; }
-
-
-   function removescreen(){
-    document.querySelector(".ecran").value=""
-}
-
-function calcul(){ // touche '='
-
-    var a = document.querySelector(".ecran").value;  
-    document.querySelector(".ecran").value = eval(a);
-}
-
-
-function sqr(){
-
-    document.querySelector(".ecran").value = Math.sqrt(document.querySelector(".ecran").value)
-}
-
